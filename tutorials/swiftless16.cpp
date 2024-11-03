07b170e8d957ed8964c74da08376b677d51df34a
Alberto Mardegan <mardy@users.sourceforge.net>
2024-11-03 09:35:07 +0300

    tutorials: add color to swiftless16 (texture)
    In this way we can see if the modulate function really works.

--- tutorials/swiftless16.cpp
+++ tutorials/swiftless16.cpp
@@ -90,6 +90,7 @@ void display (void) {
     glLoadIdentity();
     glEnable( GL_TEXTURE_2D );
     gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glColor3f(1.0, 0.5, 0.0);
     square();
     glutSwapBuffers();
     angle ++;
